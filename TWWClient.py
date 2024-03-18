77581dae36a7b47de1c34c7bcbf50fe3b9096ebd
Jonathan Tan <jontan_@hotmail.com>
2024-03-18 12:43:35 -0400

    Change how the client gives player items to use new `get_item_array`
    This method uses an asm patch to add an array that the game checks every frame. If you place an item ID in the array, the game will give the player that item.
    So, the client only needs to place the item IDs of the item it wants to give the player in that array in RAM.
    This greatly simplifies the client code because it no longer has to manually set bits in memory or keep track of various addresses.

--- TWWClient.py
+++ TWWClient.py
@@ -3,11 +3,12 @@ import time
 import traceback
 from typing import Any
 
+import dolphin_memory_engine
+
 import Utils
 from CommonClient import ClientCommandProcessor, CommonContext, get_base_parser, gui_enabled, logger, server_loop
 from NetUtils import ClientStatus, NetworkItem
 
-import dolphin_memory_engine
 from .Items import ITEM_TABLE, LOOKUP_ID_TO_NAME, TWWItem
 from .Locations import LOCATION_TABLE, TWWLocation, TWWLocationType
 
@@ -24,34 +25,12 @@ CONNECTION_INITIAL_STATUS = "Dolphin connection has not been initiated."
 # We re-purpose the small key counter and dungeon flag bytes for Ganon's Tower for this
 EXPECTED_INDEX_ADDR = 0x803C50C8
 
-SPOILS_BASE_ADDR = 0x803C4C7E
-BAIT_BASE_ADDR = 0x803C4C86
-BAIT_COUNT_BASE_ADDR = 0x803C4CAC
 LETTER_BASE_ADDR = 0x803C4C8E
 LETTER_OWND_ADDR = 0x803C4C98
-CHARTS_BASE_ADDR = 0x803C4CDC
 
-TOTG_RAISED_ADDR = 0x803C524A
-
-PICTO_BOX_IDS = [0x23, 0x26]
-BOW_IDS = [0x27, 0x35, 0x36]
-SWORD_IDS = [0x38, 0x39, 0x3A, 0x3E]
-SHIELD_IDS = [0x3B, 0x3C]
-BOTTLE_ADDRS = [0x803C4C52, 0x803C4C53, 0x803C4C54, 0x803C4C55]
-
-WALLET_ADDR = 0x803C4C1A
-MAGIC_METER_ADDR_1 = 0x803C4C1B
-MAGIC_METER_ADDR_2 = 0x803C4C1C
-
-GIVE_HEALTH_ADDR = 0x803CA764
-GIVE_RUPEE_ADDR = 0x803CA768
-GIVE_SKS_ADDR = 0x803CA77D
-
-MAX_HEALTH_ADDR = 0x803C4C08
 CURR_HEALTH_ADDR = 0x803C4C0A
 
 STAGE_INFO_ADDR = 0x803C4F88
-CURR_STAGE_INFO_ADDR = 0x803C5380
 CURR_STAGE_NAME_ADDR = 0x803C9D3C
 CURR_STAGE_ID_ADDR = 0x803C53A4
 
@@ -61,8 +40,8 @@ SEA_ALT_BITFLD_ADDR = 0x803C4FAC
 CHESTS_BITFLD_ADDR = 0x803C5380
 SWITCHES_BITFLD_ADDR = 0x803C5384
 PICKUPS_BITFLD_ADDR = 0x803C5394
-CURR_STAGE_SKS_ADDR = 0x803C53A0
-CURR_STAGE_DUNGEON_FLAGS_ADDR = 0x803C53A1
+
+GIVE_ITEM_ARRAY_ADDR = 0x803FE868
 
 
 class TWWCommandProcessor(ClientCommandProcessor):
@@ -88,6 +67,8 @@ class TWWContext(CommonContext):
         self.dolphin_status = CONNECTION_INITIAL_STATUS
         self.last_rcvd_index = -1
         self.has_send_death = False
+
+        self.len_give_item_array = 4
 
     async def disconnect(self, allow_autoreconnect: bool = False):
         self.auth = None
@@ -136,20 +117,6 @@ def write_short(console_address: int, value: int):
     dolphin_memory_engine.write_bytes(console_address, value.to_bytes(2))
 
 
-def _count_expected_heart_pieces(ctx: TWWContext):
-    heart_pieces, heart_containers = 0, 0
-    for item in ctx.items_received:
-        if item.item == TWWItem.get_apid(ITEM_TABLE["Piece of Heart"].code):
-            heart_pieces += 1
-        elif item.item == TWWItem.get_apid(ITEM_TABLE["Heart Container"].code):
-            heart_containers += 1
-
-    # Player starts with 12 heart pieces in the base randomizer
-    starting_heart_pieces = 3 * 4
-
-    return starting_heart_pieces + heart_pieces + heart_containers * 4
-
-
 def _give_death(ctx: TWWContext):
     if (
         ctx.slot
@@ -161,229 +128,35 @@ def _give_death(ctx: TWWContext):
         write_short(CURR_HEALTH_ADDR, 0)
 
 
-def _give_item(address: int | None, value: int | None, owned_address: int | None, bit_to_set: int | None):
-    if address is not None:
-        assert value is not None
-        dolphin_memory_engine.write_byte(address, value)
-
-    if owned_address is not None:
-        assert bit_to_set is not None
-        current_value = dolphin_memory_engine.read_byte(owned_address)
-        dolphin_memory_engine.write_byte(owned_address, current_value | 1 << bit_to_set)
-
-
-def _give_spoil(address: int, spoil_id: int):
-    # The Spoils Bag has 8 slots. If the given spoil is already owned, increment its counter by one. Otherwise, add the
-    # spoil to next available slot.
-    for offset in range(8):
-        slot = dolphin_memory_engine.read_byte(SPOILS_BASE_ADDR + offset)
-        if slot == spoil_id or slot == 0xFF:
-            # Set Spoils Bag slot to that spoil type
-            dolphin_memory_engine.write_byte(SPOILS_BASE_ADDR + offset, spoil_id)
-
-            # Increment counter for that spoil by 1
-            dolphin_memory_engine.write_byte(address, dolphin_memory_engine.read_byte(address) + 1)
-
-            break
-
-
-def _give_bait(bait_id: int):
-    # The Bait Bag has 8 slots. Put the bait in the next available slot and set its count to 3.
-    for offset in range(8):
-        if dolphin_memory_engine.read_byte(BAIT_BASE_ADDR + offset) == 0xFF:
-            # Set Bait Bag slot to that bait type
-            dolphin_memory_engine.write_byte(BAIT_BASE_ADDR + offset, bait_id)
-
-            # Set count to 3, even for Hyoi Pears (slot will be set to 0xFF after single use)
-            dolphin_memory_engine.write_byte(BAIT_COUNT_BASE_ADDR + offset, 3)
-
-            break
-
-
-def _give_letter(value: int, bit_to_set: int):
-    # Don't give the letter if the player has already received it
-    if (dolphin_memory_engine.read_word(LETTER_OWND_ADDR) >> bit_to_set) & 1:
-        return
-
-    # The Delivery Bag has 8 slots. Put the item in the next available slot.
-    for offset in range(8):
-        if dolphin_memory_engine.read_byte(LETTER_BASE_ADDR + offset) == 0xFF:
-            dolphin_memory_engine.write_byte(LETTER_BASE_ADDR + offset, value)
-            break
-
-    # Also, set the flag that the letter has been owned
-    current_value = dolphin_memory_engine.read_word(LETTER_OWND_ADDR)
-    dolphin_memory_engine.write_word(LETTER_OWND_ADDR, current_value | 1 << bit_to_set)
-
-
-def _give_pearl(address: int, value: int, owned_address: int, bit_to_set: int):
-    current_value = dolphin_memory_engine.read_byte(address)
-    dolphin_memory_engine.write_byte(address, current_value | value)
-
-    current_value = dolphin_memory_engine.read_byte(owned_address)
-    dolphin_memory_engine.write_byte(owned_address, current_value | 1 << bit_to_set)
-
-    # Raise TotG if player has all three pearls
-    if dolphin_memory_engine.read_byte(address) & 0xD0 == 0xD0:
-        dolphin_memory_engine.write_byte(TOTG_RAISED_ADDR, dolphin_memory_engine.read_byte(TOTG_RAISED_ADDR) | 0x40)
-
-
-def _give_other_dungeon_item(stage_id: int, item_bit: int):
-    stage_info_addr = STAGE_INFO_ADDR + 0x24 * stage_id
-
-    # address + 0x21 is the bitfield of dungeon-specific flags for this dungeon
-    current_value = dolphin_memory_engine.read_byte(stage_info_addr + 0x21)
-    dolphin_memory_engine.write_byte(stage_info_addr + 0x21, current_value | 1 << item_bit)
-
-    # If the player is currently in that dungeon, update current stage bits as well
-    curr_stage_id = dolphin_memory_engine.read_byte(CURR_STAGE_ID_ADDR)
-    if curr_stage_id == stage_id:
-        current_value = dolphin_memory_engine.read_byte(CURR_STAGE_INFO_ADDR + 0x21)
-        dolphin_memory_engine.write_byte(CURR_STAGE_INFO_ADDR + 0x21, current_value | 1 << item_bit)
-
-
-def _give_small_key(stage_id: int):
-    stage_info_addr = STAGE_INFO_ADDR + 0x24 * stage_id
-    curr_stage_id = dolphin_memory_engine.read_byte(CURR_STAGE_ID_ADDR)
-
-    if curr_stage_id == stage_id:
-        # If the player is currently in that dungeon, give the player a small key for that dungeon
-        dolphin_memory_engine.write_byte(GIVE_SKS_ADDR, 1)
-    else:
-        # address + 0x20 is the current number of small keys the dungeon
-        current_value = dolphin_memory_engine.read_byte(stage_info_addr + 0x20)
-        dolphin_memory_engine.write_byte(stage_info_addr + 0x20, current_value + 1)
-
-
-def _give_chart(owned_address: int, bit_to_set: int):
-    current_value = dolphin_memory_engine.read_word(owned_address)
-    dolphin_memory_engine.write_word(owned_address, current_value | 1 << bit_to_set)
-
-
-def _give_rupees(amount: int):
-    dolphin_memory_engine.write_word(GIVE_RUPEE_ADDR, amount)
-
-
-def _give_heart_pieces(max_hp: int):
-    # Ensure max HP is valid
-    max_hp = min(max_hp, 20 * 4)
-
-    write_short(MAX_HEALTH_ADDR, max_hp)
-
-    # Full heal the player when they receive a Piece of Heart or Heart Container
-    dolphin_memory_engine.write_float(GIVE_HEALTH_ADDR, float(20 * 4))
-
-
-def _give_item_progressive(address: int, owned_bitfield: int, item_ids: list[int]):
-    # Calculate the next amount of the progressive item
-    next_amount = dolphin_memory_engine.read_byte(owned_bitfield).bit_length()
-    next_amount = min(next_amount, len(item_ids) - 1)
-
-    dolphin_memory_engine.write_byte(address, item_ids[next_amount])
-    dolphin_memory_engine.write_byte(owned_bitfield, 1 << next_amount)
-
-
-def _give_capacities_progressive(maximum_address: int, count_address: int, capacities: list[int]):
-    current_max = dolphin_memory_engine.read_byte(maximum_address)
-    if current_max == capacities[-1]:
-        return
-
-    next_max = capacities[capacities.index(current_max) + 1]
-    dolphin_memory_engine.write_byte(maximum_address, next_max)
-    dolphin_memory_engine.write_byte(count_address, next_max)
-
-
-def _give_wallet_progressive():
-    # Calculate the next amount of the progressive wallet
-    next_amount = dolphin_memory_engine.read_byte(WALLET_ADDR).bit_length()
-    next_amount = min(next_amount, 2)
-
-    dolphin_memory_engine.write_byte(WALLET_ADDR, 1 << next_amount)
-
-
-def _give_bottle_progressive():
-    for addr in BOTTLE_ADDRS:
-        if dolphin_memory_engine.read_byte(addr) == 0xFF:
-            # 0x50 is empty bottle contents
-            _give_item(addr, 0x50, None, None)
-            break
-
-
-def _give_item_by_name(ctx: TWWContext, item: str):
-    assert item in ITEM_TABLE, f"Unknown item: {item}"
-
-    if check_ingame():
-        data = ITEM_TABLE[item]
-        match data.type:
-            case "Item":
-                _give_item(data.address, data.value, data.owned_bitfield, data.bit_to_set)
-
-            case "Spoil":
-                _give_spoil(data.address, data.value)
-
-            case "Bait":
-                _give_bait(data.value)
-
-            case "Letter":
-                _give_letter(data.value, data.bit_to_set)
-
-            case "Pearl":
-                _give_pearl(data.address, data.value, data.owned_bitfield, data.bit_to_set)
-
-            case "Prog":
-                match item:
-                    case "Progressive Sword":
-                        _give_item_progressive(data.address, data.owned_bitfield, SWORD_IDS)
-
-                    case "Progressive Shield":
-                        _give_item_progressive(data.address, data.owned_bitfield, SHIELD_IDS)
-
-                    case "Progressive Picto Box":
-                        _give_item_progressive(data.address, data.owned_bitfield, PICTO_BOX_IDS)
-
-                    case "Progressive Bow":
-                        _give_item_progressive(data.address, data.owned_bitfield, BOW_IDS)
-
-                    case "Progressive Magic Meter":
-                        _give_capacities_progressive(data.address, data.owned_bitfield, [0, 16, 32])
-
-                    case "Progressive Quiver" | "Progressive Bomb Bag":
-                        _give_capacities_progressive(data.address, data.owned_bitfield, [30, 60, 99])
-
-                    case "Progressive Wallet":
-                        _give_wallet_progressive()
-
-                    case "Empty Bottle":
-                        _give_bottle_progressive()
-
-            case "Chart":
-                _give_chart(data.owned_bitfield, data.bit_to_set)
-
-            case "Rupee":
-                _give_rupees(data.value)
-
-            case "Heart":
-                _give_heart_pieces(_count_expected_heart_pieces(ctx))
-
-            case "BKey" | "Map" | "Compass":
-                _give_other_dungeon_item(data.value, data.bit_to_set)
-
-            case "SKey":
-                _give_small_key(data.value)
-
-            case _:
-                raise Exception(f"Unknown item type: {data.type}")
+def _give_item(ctx: TWWContext, item_name: str):
+    item_id = ITEM_TABLE[item_name].item_id
+
+    # Loop through the give item array, placing the item in an empty slot
+    for idx in range(ctx.len_give_item_array):
+        slot = dolphin_memory_engine.read_byte(GIVE_ITEM_ARRAY_ADDR + idx)
+        if not slot:
+            dolphin_memory_engine.write_byte(GIVE_ITEM_ARRAY_ADDR + idx, item_id)
+            return True
+
+    # Unable to place the item in the array, so return `False`
+    return False
 
 
 async def give_items(ctx: TWWContext):
     if check_ingame():
+        # Read the expected index of the player, which is the index of the latest item they've received
         expected_idx = read_short(EXPECTED_INDEX_ADDR)
+
+        # Loop through items to give
         for item, idx in ctx.items_received_2:
+            # If the index of the item is greater than the expected index of the player, give the player the item
             if expected_idx <= idx:
-                item_name = LOOKUP_ID_TO_NAME[item.item]
-                _give_item_by_name(ctx, item_name)
+                # Attempt to give the item and increment the expected index
+                while not _give_item(ctx, LOOKUP_ID_TO_NAME[item.item]):
+                    await asyncio.sleep(0.01)
+
+                # Increment the expected index
                 write_short(EXPECTED_INDEX_ADDR, idx + 1)
-                # await asyncio.sleep(0.01)  # wait a bit for values to update
 
 
 async def check_locations(ctx: TWWContext):
